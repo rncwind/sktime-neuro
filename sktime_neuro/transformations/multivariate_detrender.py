676504cc31dce2bff14b80de0bdec0c7092be76d
rncwind <rncnwd@tutanota.com>
2022-04-25 23:09:12 +0100

    ENH: Multivariate Detrender and fixed Motor Imagery Example.
    This took a surprising amount of debugging and reading in order to get working.

--- sktime_neuro/transformations/multivariate_detrender.py
+++ sktime_neuro/transformations/multivariate_detrender.py
@@ -1,10 +1,14 @@
 import sktime.datasets
 from sktime.forecasting.compose import ColumnEnsembleForecaster
 from sktime.forecasting.trend import PolynomialTrendForecaster
+from sktime.forecasting.base._fh import ForecastingHorizon
 from sktime.transformations.series.detrend import Detrender
+from joblib import Parallel, delayed
 from typing import *
+from copy import deepcopy
 
 import pandas as pd
+import numpy as np
 
 """
 Unfortunatley, EEG data is multivariate. SKTime used to support this out of the box
@@ -20,42 +24,103 @@ class ColumnEnsembleDetrender:
     to apply trendforecasters.
     """
 
-    def __init__(self, dataset : Union[pd.Series, pd.DataFrame], forecaster=None):
-        self.forecaster = forecaster
-        self.forecasters = []
+    def __init__(self, dataset: np.ndarray):
         self.dataset = dataset
-        self.fh = self.dataset.shape[1]
-        if forecaster is None:
-            self.forecaster = PolynomialTrendForecaster()
+        self.fitted = False
+        self.forecasters = []
+        for column in range(dataset.shape[1]):
+            self.forecasters.append(
+                (("trend" + str(column)), PolynomialTrendForecaster(), column)
+            )
+        self.cef = ColumnEnsembleForecaster(forecasters=self.forecasters)
+        self.fh = np.arange(self.dataset.shape[0])
 
-        self.dup_forecasters()
+    def fit(self):
+        if self.fitted is False:
+            self.cef.fit(self.dataset, fh=self.fh)
 
-    def dup_forecasters(self):
-        for i in range(0, self.fh):
-            name = f"forecaster{i}"
-            tup = (name, self.forecaster, i)
-            self.forecasters.append(tup)
-        print(f"Created {i} forecasters")
+    def fit_predict(self):
+        if self.fitted is False:
+            self.cef.fit(self.dataset, fh=self.fh)
+        return self.cef.predict()
 
-    def detrend(self):
-        fc = ColumnEnsembleForecaster(forecasters=self.forecasters)
-        fc.fit(self.dataset, fh=list(range(1, self.fh)))
-        return fc.predict()
 
 class DumbDetrender:
     """Just apply detrend to each column on it's own!"""
-    def __init__(self, dataset: Union[pd.Series, pd.DataFrame], forecaster = None):
+    def __init__(self, dataset: pd.DataFrame, forecaster = None):
         self.dataset = dataset
         if forecaster is None:
             self.forecaster = PolynomialTrendForecaster(degree=1)
         else:
             self.forecaster = forecaster
 
-    def detrend(self):
-        transformed = pd.DataFrame()
-        transformer = Detrender(forecaster=self.forecaster)
-        for name, values in self.dataset.items():
-            detrendcol = transformer.fit_transform(values)
-            transformed = pd.concat([transformed, detrendcol], axis=1)
 
-        return transformed
+    def detrend(self, n_jobs: Optional[int]):
+        """
+        n_jobs : Number of jobs to run in parallel, default is 4.
+        """
+        if n_jobs is None:
+            n_jobs = 4
+        processed_list = []
+        if isinstance(self.dataset, np.ndarray):
+            print("Ndarray")
+            processed_list = Parallel(n_jobs=n_jobs)(delayed(detrend_col)(values, deepcopy(self.forecaster)) for values in self.dataset.T)
+        elif isinstance(self.dataset, pd.DataFrame):
+            print("Dataframe")
+            detrended_cols = Parallel(n_jobs=4)(delayed(detrend_col)(values, self.forecaster) for name, values in self.dataset)
+
+        if isinstance(self.dataset, np.ndarray):
+            con = np.empty_like(self.dataset)
+            for detrended_col in processed_list:
+                np.concatenate(con, detrended_col)
+            return con
+        elif isinstance(self.dataset, pd.DataFrame):
+            raise NotImplementedError("Only numpy arrays for now")
+
+
+
+def detrend_col(values, forecaster):
+    transformer = Detrender(forecaster=forecaster)
+    detrendcol = transformer.fit_transform(values)
+    print("Done")
+    return detrendcol
+
+
+def test_dumb_detrender():
+    from pooch import os_cache
+    from mne_bids import BIDSPath, read_raw_bids
+    from sktime_neuro.transformations.series.seriesdownsampling import SeriesDownsampling
+
+    root = os_cache("sktime_neuro") / "eeg_matchingpennies"
+    bids_path = BIDSPath(
+        subject="08", task="matchingpennies", suffix="eeg", datatype="eeg", root=root
+    )
+
+    raw = read_raw_bids(bids_path=bids_path, verbose=False)
+
+    # mne assumes shape channels*timepoints;sktime assumes shape timepoints*channels
+    data = raw.get_data().transpose()
+    downsampled_data = SeriesDownsampling(2).fit_transform(data)
+    dt = DumbDetrender(downsampled_data)
+    detrended = dt.detrend(n_jobs=8)
+    assert(detrended is not None)
+
+def test_ensemble():
+    from pooch import os_cache
+    from mne_bids import BIDSPath, read_raw_bids
+    from sktime_neuro.transformations.series.seriesdownsampling import SeriesDownsampling
+
+    root = os_cache("sktime_neuro") / "eeg_matchingpennies"
+    bids_path = BIDSPath(
+        subject="08", task="matchingpennies", suffix="eeg", datatype="eeg", root=root
+    )
+
+    raw = read_raw_bids(bids_path=bids_path, verbose=False)
+
+    # mne assumes shape channels*timepoints;sktime assumes shape timepoints*channels
+    data = raw.get_data().transpose()
+    downsampled_data = SeriesDownsampling(2).fit_transform(data)
+
+    dt = ColumnEnsembleDetrender(downsampled_data)
+    detrended = dt.fit_predict()
+    print("A")
